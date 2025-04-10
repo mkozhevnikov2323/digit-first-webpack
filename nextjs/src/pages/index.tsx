baa4116aecff8fde4f942e4b487900a40c41aae8
mkozhevnikov2323 <mkozhevnikov23@gmail.com>
2025-04-10 20:53:32 +0400

    add Panda CSS, postcss.config, components Card, Button with CSS modules and CSS-in-JS

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,12 +1,18 @@
+import '../app/globals.css';
+
 import React from 'react';
 
+import { Card } from '@/components/Card';
 import { Hello } from '@/components/Hello';
+
+import { css } from '../../styled-system/css';
 
 export default function Home() {
   return (
-    <main>
+    <main className={css({ fontSize: '2xl', p: '20px' })}>
       <h1>Welcome to Next.js with Aliases!</h1>
       <Hello />
+      <Card />
     </main>
   );
 }
