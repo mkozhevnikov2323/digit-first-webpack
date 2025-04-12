c16861456943d9a8e864ac85f1f7f5b457cffb7e
mkozhevnikov2323 <mkozhevnikov23@gmail.com>
2025-04-12 18:04:03 +0400

    add page posts, AntD, fetch post from jsonplaceholder

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,7 @@ export default [
         module: 'readonly',
         NodeJS: true,
         require: 'readonly',
+        fetch: 'readonly',
       },
       parser: tsParser,
       parserOptions: {
@@ -64,6 +65,7 @@ export default [
       ],
       'react/prop-types': 'off',
       '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/no-explicit-any': ['off'],
     },
   },
   js.configs.recommended,
